a9fa4da61c5012dcc22c050c9ba62d628c62937f
Quinn Elder <qre.turing@gmail.com>
2020-10-16 11:45:01 -0600

    Refactor for post and get methods

--- MEAN-STACK/backend/app.js
+++ MEAN-STACK/backend/app.js
@@ -1,8 +1,28 @@
 const express = require('express')
 
+const bodyParser = require('body-parser')
+
 const app = express()
 
-app.use('/api/posts', (req, res, next) => {
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', "*")
+  res.setHeader('Access-Control-Allow-Headers', "Origin, X-Request-With, Content-Type, Accept")
+  res.setHeader('Access-Control-Allow-Methods', "GET, POST, PATCH, DELETE, PUT, OPTIONS")
+  next()
+})
+
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body
+  console.log(post)
+  res.status(201).json({
+    message: 'Post was added'
+  })
+})
+
+app.get('/api/posts', (req, res, next) => {
   const posts = [{
     title: 'First server side post',
     content: 'Coming from server',
@@ -14,7 +34,7 @@ app.use('/api/posts', (req, res, next) => {
     id: "2",
   }]
   res.status(200).json({
-    message: 'Posts fetched succesfully',
+    message: 'Posts fetched successfully',
     posts: posts
   })
 })
