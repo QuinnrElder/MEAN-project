21d6e7eaff6809974fdd0d9807220a3dd2cf90c6
Quinn Elder <qre.turing@gmail.com>
2020-10-16 10:41:00 -0600

    Install nodemon

--- MEAN-STACK/backend/app.js
+++ MEAN-STACK/backend/app.js
@@ -2,13 +2,21 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log('first')
-  next()
-})
-
-app.use((req, res, next) => {
-  res.send("Hello Quinn")
+app.use('/api/posts', (req, res, next) => {
+  const posts = [{
+    title: 'First server side post',
+    content: 'Coming from server',
+    id: "1",
+  },
+  {
+    title: 'Second server side post',
+    content: 'Coming from server',
+    id: "2",
+  }]
+  res.status(200).json({
+    message: 'Posts fetched succesfully',
+    posts: posts
+  })
 })
 
 module.exports = app
